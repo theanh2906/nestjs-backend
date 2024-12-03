22c453371fcdf836c4a780accfcb5cdca790cc1e
Anh Tang <anh.tang@opswat.com>
2024-12-03 18:24:49 +0700

    add monitor

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:4200'],
+      origin: ['http://localhost:4200', 'https://benna.vercel.app'],
       methods: ['GET', 'POST', 'PATCH', 'PUT'],
     },
   });
